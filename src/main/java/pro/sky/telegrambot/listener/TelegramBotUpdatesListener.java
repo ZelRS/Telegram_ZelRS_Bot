bbad9dbf8c37fd233f2d65d3ef489fc6f931bf20
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-11-02 15:47:24 +0600

    Научил бота реагировать и отвечать на команды. Подключил и настроил библиотеку Liquibase для работы с БД.

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +18,11 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    @Autowired
-    private TelegramBot telegramBot;
+    private final TelegramBot telegramBot;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
 
     @PostConstruct
     public void init() {
@@ -29,8 +34,32 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
 
+//          получаем запрос от клиента и кладем в переменную
+            String updateText = update.message().text();
+//          получаем уникальный идентификатор чата и кладем в переменную
+            Long chatId = update.message().chat().id();
+
+//          формирование ответа вынесено в отдельный метод responseMaker()
+            switch (updateText) {
+                case "/start":
+                    responseMaker(chatId, "Привет, пользователь!");
+                    break;
+                case "/help":
+                    responseMaker(chatId, "Чем тебе помочь?");
+                    break;
+                default:
+                    responseMaker(chatId, "Я не знаю такой команды...");
+                    break;
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    private void responseMaker(Long chatId, String responseMessage) {
+//        формируем отправляемое сообщение
+        SendMessage sendMessage = new SendMessage(chatId, responseMessage);
+//        отправляем ответ с сообщением клиенту
+        telegramBot.execute(sendMessage);
     }
 
 }
