3674fde0cef2b6590acab0454cf334f31d044fd6
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-11-02 16:59:57 +0600

    Создал таблицу notification_task при помощи Liquibase. Создал сущность notificationTask, соответствующую данной таблице. Создал интерфейс репозитория для новой сущности.

--- src/main/resources/liquibase/scripts/script1.sql
+++ src/main/resources/liquibase/scripts/script1.sql
@@ -1,2 +1,17 @@
 -- liquibase formatted sql
 
+-- changeset Zelenin_Roman:1
+CREATE TABLE notification_task
+(
+    id BIGINT PRIMARY KEY,
+    chatId BIGINT CHECK (chatId > 0),
+    responseMessage TEXT NOT NULL,
+    date_time TIMESTAMP WITH TIME ZONE
+);
+
+-- changeset Zelenin_Roman:2
+ALTER TABLE notification_task RENAME COLUMN responsemessage TO notification_message;
+ALTER TABLE notification_task RENAME COLUMN chatId  TO chat_id;
+
+
+
